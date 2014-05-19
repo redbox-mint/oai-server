3c52eb0d529f04c56a44667e6e39e2788cb0dee6
shilob <shilo@redboxresearchdata.com.au>
2014-05-19 14:35:18 +1000

    REDBOX-262 #comment  Corrected environment variable

--- support/install/setenv.sh
+++ support/install/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS='-XX:PermSize=64M -XX:MaxPermSize=512M' -DcmConfigPath=/var/local/curationmanager/
+JAVA_OPTS"-XX:PermSize=64M -XX:MaxPermSize=512M -DcmConfigPath=/var/local/curationmanager/"

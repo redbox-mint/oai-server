248056de6e0bf2f39ba45a28d527ce1a50871ad1
Shilo Banihit <shilo@redboxresearchdata.com.au>
2014-10-29 13:14:53 +1000

    RPP-50 #comment Bumping Tomcat's initial required memory to 2GB due to increased usage from Harvesters.

--- support/install/setenv.sh
+++ support/install/setenv.sh
@@ -1,2 +1,2 @@
-JAVA_OPTS="-XX:PermSize=64M -XX:MaxPermSize=512M -DcmConfigPath=/var/local/curationmanager/"
+JAVA_OPTS="-Xms2048m -Xmx4096m -XX:PermSize=2048M -XX:MaxPermSize=4096M -DcmConfigPath=/var/local/curationmanager/"
 CATALINA_PID=/var/run/tomcat/tomcat7.pid
